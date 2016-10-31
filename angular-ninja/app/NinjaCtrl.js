a3052804629ea4e9385e7bd680a56d0889dd3c1a
itesic <ilija.tesic@gmail.com>
2016-10-31 23:44:41 +0100

    Finished assignment 1

--- angular-ninja/app/NinjaCtrl.js
+++ angular-ninja/app/NinjaCtrl.js
@@ -1,5 +1,9 @@
 angular
   .module('ninja')
   .controller('NinjaCtrl', function ($scope) {
-    $scope.ime = "Ilija";
+    $scope.name = "Mačak Tošo";
+    $scope.imagePath = "assets/img/profile-image.jpg";
+    $scope.birthday = "01.01.1990";
+    $scope.email = "macak.toso@gmail.com";
+    $scope.skype = "macak.toso"
   });
