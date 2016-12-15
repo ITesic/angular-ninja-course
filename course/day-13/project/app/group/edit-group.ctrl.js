3a4973b39cc5624434ce2bb3dc96da396c6bad26
itesic <ilija.tesic@gmail.com>
2016-12-15 18:14:18 +0100

    Finis assignment for day 13

--- course/day-13/project/app/group/edit-group.ctrl.js
+++ course/day-13/project/app/group/edit-group.ctrl.js
@@ -3,10 +3,15 @@ angular
   .controller('EditGroupCtrl', function($scope, $state, $stateParams, groupService){
     $scope.group = $stateParams.group;
 
+    $scope.errors = [];
+
     $scope.saveGroup = function(group) {
       groupService.updateGroup($scope.group.id, $scope.group)
         .then(function(group){
           $state.go('groups');
         })
+        .catch(function(e){
+          $scope.errors = e.data.errors;
+        });
     }
   });
